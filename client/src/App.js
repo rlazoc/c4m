74fe09d4e8ad903033d83d64b6b8db990e5b4d33
Roxana Lazo-Chavez <roxanalazo10@gmail.com>
2023-02-26 16:36:35 -0600

    Add state logic for login

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,38 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
   <div className="App">
     <h1>Connect4</h1>
+    <form>
+      <label>
+        Username:&nbsp;
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <label>
+        Password:&nbsp;
+        <input type="text" value={password} onChange={handlePasswordChange} />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
    </div>
   );
+  
 }
 
 export default App;
